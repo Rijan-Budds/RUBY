c1ab9bfd4621903afaa43bf04cf2d720383d6a63
Rijan-Budds <buddsrijan@gmail.com>
2026-03-16 10:53:42 +0545

    first commit

---
+++ new.rb
@@ -0,0 +1,8 @@
+def say_hello(name)
+    result = "I don't know why you said goodbye #{name.upcase.strip}." + " I said hello."
+    return result
+end
+
+puts say_hello(" rijan ")
+
+puts "ying\nand\nyang"
